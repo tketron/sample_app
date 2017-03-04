41a008b9d58487cb9493e6c375b1cc1313a92f47
Tyler Ketron <tketron@gmail.com>
2017-03-04 13:24:51 -0800

    add model and databse validations

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
